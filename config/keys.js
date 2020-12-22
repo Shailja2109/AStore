9146233531d99581ac9ebe5ce5aeb9f2641d5978
Shailja <shailjaatkotiya@gmail.com>
2020-12-22 13:27:22 +0530

    login and register functionality added

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://AStore_admin:AStore123@cluster0.8nvdb.mongodb.net/AStore?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const app = express();
+
+const users = require('./routes/api/users.js');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+  
+app.get('/', (req, res)=> res.send('Hello u'));
+
+app.use('/api/users', users);
+
+const port = process.envPORT || 3000;
+
+app.listen(port, ()=> console.log(
+  'server running on port', port
+))
