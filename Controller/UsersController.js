59c8255e3e5fb1519fb718cf266d5cafbc36116b
harshil74 <harshilgohel723@gmail.com>
2021-01-06 20:30:56 +0530

    made function for admin auth

--- Controller/UsersController.js
+++ Controller/UsersController.js
@@ -8,6 +8,8 @@ const validateLoginInput = require('../validation/login');
 
 const User = require('../models/User');
 
+
+// customer login
 exports.userlogin =  (req, res) => {
     const { errors, isValid } = validateLoginInput(req.body);
   
@@ -46,6 +48,7 @@ exports.userlogin =  (req, res) => {
     });
 }
 
+// customer registration 
 exports.userRegister = (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
   
@@ -84,4 +87,86 @@ exports.userRegister = (req, res) => {
         });
       }
     });
+}
+
+//admin registration 
+exports.adminRegistration = (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '250',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        contact: req.body.contact,
+        avatar,
+        role:keys.ADMIN,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+}
+
+
+// admin login
+exports.adminLogIn = (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
+    }
+    if(!(user.role === keys.ADMIN)){
+      errors.inValidAccess = 'User not admin, can not login';
+      return res.status(404).json(errors);
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = { id: user.id, name: user.name, avatar: user.avatar,role:user.role };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 36000 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
+      } else {
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
+      }
+    });
+  });
 }
--- routes/admin/admin_auth.js
+++ routes/admin/admin_auth.js
@@ -1,101 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys');
 const passport = require('passport');
 
-const validateRegisterInput = require('../../validation/register');
-const validateLoginInput = require('../../validation/login');
 const checkAdminUser = require('../../validation/check-user');
 
-const User = require('../../models/User');
+const UserController = require('../../Controller/UsersController')
 
 // @route   GET admin/register
 // @desc    Register admin
 // @access  Public
-router.post('/register', (req, res) => {
-    const { errors, isValid } = validateRegisterInput(req.body);
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+router.post('/register',UserController.adminRegistration);
   
-    User.findOne({ email: req.body.email }).then(user => {
-      if (user) {
-        errors.email = 'Email already exists';
-        return res.status(400).json(errors);
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: '250',
-          r: 'pg',
-          d: 'mm'
-        });
-  
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          contact: req.body.contact,
-          avatar,
-          role:keys.ADMIN,
-          password: req.body.password
-        });
-  
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
-      }
-    });
-  });
-  
-  // @route   GET admin/login
-  // @desc    Login admin User / Returning JWT Token
-  // @access  Public
-  router.post('/login', (req, res) => {
-    const { errors, isValid } = validateLoginInput(req.body);
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
-    const email = req.body.email;
-    const password = req.body.password;
-    User.findOne({ email }).then(user => {
-      if (!user) {
-        errors.email = 'User not found';
-        return res.status(404).json(errors);
-      }
-      if(!(user.role === 'admin')){
-        errors.email = 'User not admin, can not login';
-        return res.status(404).json(errors);
-      }
-  
-      bcrypt.compare(password, user.password).then(isMatch => {
-        if (isMatch) {
-          const payload = { id: user.id, name: user.name, avatar: user.avatar,role:user.role };
-          jwt.sign(
-            payload,
-            keys.secretOrKey,
-            { expiresIn: 36000 },
-            (err, token) => {
-              res.json({
-                success: true,
-                token: 'Bearer ' + token
-              });
-            }
-          );
-        } else {
-          errors.password = 'Password incorrect';
-          return res.status(400).json(errors);
-        }
-      });
-    });
-  });
+// @route   GET admin/login
+// @desc    Login admin User / Returning JWT Token
+// @access  Public
+router.post('/login', UserController.adminLogIn);
 
 
 // @route   GET admin/users/dashboard
@@ -105,10 +24,8 @@ router.get(
     '/dashboard',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
-      console.log(req)
         const { errors, isValid } = checkAdminUser(req.user);
         if (!isValid) {
-          res.json({msg: 'some error'})
             return res.status(400).json(errors);
         }
         res.json({
