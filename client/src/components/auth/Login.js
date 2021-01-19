ed9a3e76e6edf9a2681908405b2fae161ed307a7
harshil74 <harshilgohel723@gmail.com>
2021-01-19 16:28:51 +0530

    login register for customer with validation

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,16 +1,81 @@
 import React, { Component } from "react";
 import trolly from "../../images/trolly3.jpg";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import classnames from "classnames";
 
 class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/api/users/login", user)
+      .then((res) => console.log(res.data))
+      .catch((err) => this.setState({ errors: err.response.data }));
+  }
   render() {
+    const { errors } = this.state;
     return (
       <div className="login-div">
         <div className="login-form">
           <h1>Astore</h1>
-          <h4>LogIn to your account </h4>
-          <form>
-            <input type="text" />
+          <h4>LogIn to your account</h4>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                className={classnames("form-control", {
+                  "is-invalid": errors.email,
+                })}
+                placeholder="Email"
+                name="email"
+                value={this.state.email}
+                onChange={this.onChange}
+              />
+              {errors.email && (
+                <div className="invalid-feedback">{errors.email}</div>
+              )}
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className={classnames("form-control", {
+                  "is-invalid": errors.password,
+                })}
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+              {errors.password && (
+                <div className="invalid-feedback">{errors.password}</div>
+              )}
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
           </form>
+          <p>
+            New to Astore? <Link to="/register">Create an account.</Link>
+          </p>
         </div>
         <div className="login-img">
           <img src={trolly} alt="Store" />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,143 @@
 import React, { Component } from "react";
+import trolly from "../../images/trolly2.jpg";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import classnames from "classnames";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      contact: "",
+      password: "",
+      password2: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const user = {
+      name: this.state.name,
+      email: this.state.email,
+      contact: this.state.contact,
+      password: this.state.password,
+      password2: this.state.password2,
+    };
+    axios
+      .post("/api/users/register", user)
+      .then((res) => console.log(res.data))
+      .catch((err) => this.setState({ errors: err.response.data }));
+  }
   render() {
-    return <div>Customer Register</div>;
+    const { errors } = this.state;
+    return (
+      <div className="login-div">
+        <div className="login-form">
+          <h1>Astore</h1>
+          <h4>Register to your account</h4>
+          <form noValidate onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className={classnames("form-control", {
+                  "is-invalid": errors.name,
+                })}
+                placeholder="name"
+                name="name"
+                value={this.state.name}
+                onChange={this.onChange}
+              />
+              {errors.name && (
+                <div className="invalid-feedback">{errors.name}</div>
+              )}
+            </div>
+
+            <div className="form-group">
+              <input
+                type="email"
+                className={classnames("form-control", {
+                  "is-invalid": errors.email,
+                })}
+                placeholder="Email"
+                name="email"
+                value={this.state.email}
+                onChange={this.onChange}
+              />
+              {errors.email && (
+                <div className="invalid-feedback">{errors.email}</div>
+              )}
+            </div>
+
+            <div className="form-group">
+              <input
+                type="text"
+                className={classnames("form-control", {
+                  "is-invalid": errors.contact,
+                })}
+                placeholder="Contact number"
+                name="contact"
+                value={this.state.contact}
+                onChange={this.onChange}
+              />
+              {errors.contact && (
+                <div className="invalid-feedback">{errors.contact}</div>
+              )}
+            </div>
+
+            <div className="form-group">
+              <input
+                type="password"
+                className={classnames("form-control", {
+                  "is-invalid": errors.password,
+                })}
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+              {errors.password && (
+                <div className="invalid-feedback">{errors.password}</div>
+              )}
+            </div>
+
+            <div className="form-group">
+              <input
+                type="password"
+                className={classnames("form-control", {
+                  "is-invalid": errors.password2,
+                })}
+                placeholder="Password"
+                name="password2"
+                value={this.state.password2}
+                onChange={this.onChange}
+              />
+              {errors.password2 && (
+                <div className="invalid-feedback">{errors.password2}</div>
+              )}
+            </div>
+
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+          </form>
+          <p>
+            Already Registered to Astore? <Link to="/login">Log in.</Link>
+          </p>
+        </div>
+        <div className="login-img">
+          <img src={trolly} alt="Store" />
+        </div>
+      </div>
+    );
   }
 }
 export default Register;
